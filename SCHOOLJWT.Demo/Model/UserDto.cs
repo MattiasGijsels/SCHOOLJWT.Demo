4fcf7990fdd949e225111e9e3439d3e732d84ce9
Mattias <Mattias@DESKTOP-KL7I237>
2025-04-02 13:35:21 +0200

    Add project files.

---
+++ SCHOOLJWT.Demo/Model/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SCHOOLJWT.Demo.Model
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
